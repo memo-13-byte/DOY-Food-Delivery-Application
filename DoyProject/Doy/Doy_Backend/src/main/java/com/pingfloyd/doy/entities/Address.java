2b3b96b61d7416a88232098cf4bed2acc7171c3f
Samet Tekin <116036418+samoosama1@users.noreply.github.com>
2025-04-11 16:34:05 +0300

    entity-dev
    changes to .gitignore

--- DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/entities/Address.java
+++ DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/entities/Address.java
@@ -1,11 +1,11 @@
 package com.pingfloyd.doy.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-/*
+import jakarta.persistence.*;
+
 @Entity
 @Table(name = "address")
 public class Address {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long address_id;
 }
-*/
