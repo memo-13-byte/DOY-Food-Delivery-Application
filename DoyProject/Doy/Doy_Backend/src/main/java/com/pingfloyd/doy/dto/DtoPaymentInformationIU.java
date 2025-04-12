6625c052cc4d1f9bc7ace7dbfcfc2104931afd2e
cetinsaid <137168667+cetinsaid@users.noreply.github.com>
2025-04-12 17:44:11 +0300

    Merge remote-tracking branch 'origin/payment-information-service' into entity-login

---
+++ DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/dto/DtoPaymentInformationIU.java
@@ -0,0 +1,35 @@
+package com.pingfloyd.doy.dto;
+
+import com.pingfloyd.doy.entities.Address;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoPaymentInformationIU {
+    private String address;
+
+    @NotEmpty(message = "Phone number must not be empty.")
+    private String phoneNumber;
+
+    @NotEmpty(message = "First name must not be empty")
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty
+    private String email;
+
+    private String notesForCourier;
+    private Integer tip;
+    @NotEmpty
+    private String cardNumber;
+    @NotEmpty
+    private String cvv;
+    @NotEmpty
+    private String expiryDate;
+    @NotEmpty
+    private String nameOnCard;
+}
