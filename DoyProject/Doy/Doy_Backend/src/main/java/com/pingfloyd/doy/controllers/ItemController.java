40e1c896541881cb8b490b74dbd8e87bc9e617c2
Barış <46918905+Baris-Yildiz@users.noreply.github.com>
2025-04-13 04:22:37 +0300

    port fix

--- DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/controllers/ItemController.java
+++ DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/controllers/ItemController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/item")
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ItemController implements IItemController {
 
     @Autowired
--- DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/controllers/RestaurantController.java
+++ DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/controllers/RestaurantController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/restaurant")
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 public class RestaurantController implements IRestaurantController {
 
     @Autowired
