60380fffc9551a9631ffa453e0085441b729e1b9
Samet Tekin <116036418+samoosama1@users.noreply.github.com>
2025-04-20 16:24:14 +0300

    new-entities-dev
    added isAvailable property in Courier

--- DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/entities/Courier.java
+++ DoyProject/Doy/Doy_Backend/src/main/java/com/pingfloyd/doy/entities/Courier.java
@@ -1,13 +1,8 @@
 package com.pingfloyd.doy.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "courier")
@@ -18,4 +13,7 @@ public class Courier extends User {
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false, orphanRemoval = true)
     @JoinColumn(name = "gid", referencedColumnName = "gid", unique = true)
     private GovernmentId governmentId;
+
+    @Column(name = "is_available")
+    private Boolean isAvailable;
 }
